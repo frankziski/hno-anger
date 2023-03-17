96f5ceae9c7d68c98d77d4250ace45de13b6d5ed
frank <frank@ziski.de>
2023-03-17 13:34:42 +0100

    added modal window to footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -16,5 +16,27 @@
 <?= $page->parent()->codeFooter() ?>
 <?php endif ?>
 
+<?php if( $site->showModal()->toBool() === true ): $modalName = 'attention'; ?>
+  <div class="modal fade" id="<?= $modalName ?>" tabindex="-1" aria-labelledby="<?= $modalName.'-label' ?>" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <?php if($site->modalTitle()->isNotEmpty()): ?>
+                <h5 class="modal-title"><?= kirbytags($site->modalTitle()->inline()) ?></h5>
+            <?php endif ?>
+          </div>
+          <div class="modal-body">            
+            <div class="modal-text">
+              <?= kirbytags($site->modalText()) ?>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-style-primary" data-bs-dismiss="modal" aria-label="Verstanden.">Verstanden.</button>
+          </div>
+        </div>
+    </div>
+  </div>
+<?php endif ?>
+
 </body>
 </html>
